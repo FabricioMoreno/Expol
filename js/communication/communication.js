d53f7e9797eab2937f94ce3b1b09146853115bac
Fabricio <fabricio.morenonaranjo@hotmail.com>
2020-09-27 19:44:18 -0500

    Implementacion de codigo para buscar el json en el db y correcion de los js de todas las subjects

--- js/communication/communication.js
+++ js/communication/communication.js
@@ -1,6 +1,7 @@
-import communicationJson from '../communication/communicationJson.js'
+import findJsonInDb from '../codes/findJsonInDb.js'
 import generatesHtml from '../codes/generatesHtmlOfSubjects.js'
 
-let jsonOfCommunication = communicationJson()
-generatesHtml(jsonOfCommunication)
+let jsonUrlOfCommunication = 'https://res.cloudinary.com/doekxasxp/raw/upload/v1601251947/jsonSubjects/communicationJson_djf8zc.json'
 
+findJsonInDb(jsonUrlOfCommunication)
+.then(json=>generatesHtml(json))
